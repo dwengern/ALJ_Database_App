ca13c3e8fc3d38b9d5428d2b11d7606b40f01ca9
Steve Spicklemire <steve@spvi.com>
2024-10-24 11:41:55 +0000

    Set up basic interaction with firebase app.

---
+++ src/lib/user-types.ts
@@ -0,0 +1,9 @@
+export type User = {
+    name: string,
+    email: string
+}
+
+export const NullUser:User = {
+    name:'',
+    email:''
+}
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,38 @@
 <script lang="ts">
-    
+    import {loadApp} from "$lib/firebase-client"
+    import type {User} from "$lib/user-types"
+    import {NullUser} from "$lib/user-types"
+    import { browser } from '$app/environment';
+
+    let user:User = $state(NullUser)
+
+    function doLogin() {
+        console.log("login")
+    }
+
+    function doLogout() {
+        console.log("logout")
+    }
+
+    if (browser) {
+        const {app, analytics} = loadApp()
+        console.log("We have app")
+    }
+
 </script>
 
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
 
-<p>This is an update to the main page.</p>
+<p>This is an update to the main page. 10/24 7:08 am</p>
+
+<p> User is Name:"{user.name}" with Email:"{user.email}"</p>
+
+{#if browser}
+    {#if user.name.length==0}
+        <button onclick = {doLogin}>Google</button>
+    {:else}
+        <button onclick={doLogout}>Logout</button>
+    {/if}
+
+{/if}
