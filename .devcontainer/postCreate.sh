a93b189a687119f05706f6f82af8d3ec1570d136
Steve Spicklemire <steve@spvi.com>
2024-10-24 10:06:18 +0000

    incorporate fb project settings.

--- .devcontainer/postCreate.sh
+++ .devcontainer/postCreate.sh
@@ -1,2 +1,4 @@
 npm install
 bash -i -c 'nvm install lts/hydrogen'
+npm install -g firebase-tools
+firebase experiments:enable webframeworks
---
+++ src/lib/firebase-client.ts
@@ -0,0 +1,24 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyC0WPpH52vz7ATRg-6bI-OCOOxDPNYmj9I",
+  authDomain: "alj-firebase.firebaseapp.com",
+  projectId: "alj-firebase",
+  storageBucket: "alj-firebase.appspot.com",
+  messagingSenderId: "930702959478",
+  appId: "1:930702959478:web:dea7a808c32870f5191057",
+  measurementId: "G-0KVXES7FRE"
+};
+
+// Initialize Firebase
+export const loadApp = () => {
+    const app = initializeApp(firebaseConfig);
+    const analytics = getAnalytics(app);
+    return {app, analytics};
+}
