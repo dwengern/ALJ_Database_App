9881ee8e6773efe2ac0aceb803062aff8ecee60d
Nicholas Dwenger <146957026+dwengern@users.noreply.github.com>
2024-11-07 13:29:47 +0000

    Merged new search code with sharedState

--- src/lib/search-feature.ts
+++ src/lib/search-feature.ts
@@ -1,26 +1,13 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 import { initializeApp } from "firebase/app";
 import type { Voyage } from '$lib/voyage-type';
+import {sharedState} from '$lib/sharedState.svelte';
 import { getFirestore, Firestore, collection, getDocs, query, where, CollectionReference } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-const firebaseConfig = {
-  apiKey: "AIzaSyC0WPpH52vz7ATRg-6bI-OCOOxDPNYmj9I",
-  authDomain: "alj-firebase.firebaseapp.com",
-  projectId: "alj-firebase",
-  storageBucket: "alj-firebase.firebasestorage.app",
-  messagingSenderId: "930702959478",
-  appId: "1:930702959478:web:dea7a808c32870f5191057",
-  measurementId: "G-0KVXES7FRE"
-};
-
-// Initialize Firebase app and Firestore
-const app = initializeApp(firebaseConfig);
-const db: Firestore = getFirestore(app);
 
 export async function getAllShips(): Promise<Voyage[]> {
     try {
-        const shipsCollectionRef = collection(db, 'voyages_db') as CollectionReference<Voyage>;
+        const shipsCollectionRef = collection(sharedState.db!, 'voyages_db') as CollectionReference<Voyage>;
         const querySnapshot = await getDocs(shipsCollectionRef);
         return querySnapshot.docs.map(doc => doc.data());
     } catch (error) {
@@ -31,7 +18,7 @@ export async function getAllShips(): Promise<Voyage[]> {
 
 export async function searchShipByName(shipName: string): Promise<Voyage[] | null> {
     try {
-        const shipsCollectionRef = collection(db, 'voyages_db') as CollectionReference<Voyage>;
+        const shipsCollectionRef = collection(sharedState.db!, 'voyages_db') as CollectionReference<Voyage>;
 
         const q = query(shipsCollectionRef, where("SHIPNAME", "==", shipName));
 
